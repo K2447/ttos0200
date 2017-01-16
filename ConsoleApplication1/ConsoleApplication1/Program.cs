e3266b2c351b73d43f20242f96ada4939da72ef4
Veerti Pekonniemi <K2447@student.jamk.fi>
2017-01-16 19:11:20 +0200

    Init commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Linq;
+
+
+namespace HT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] annetutpisteet = new int[5];
+
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("Anna pisteet: ");
+                annetutpisteet[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            int summa = annetutpisteet.Sum();
+            Console.WriteLine("Suurimmat pisteet: " + annetutpisteet.Max());
+            Console.WriteLine("Pienimmät pisteet: " + annetutpisteet.Min());
+            int pisteet = summa - annetutpisteet.Min() - annetutpisteet.Max();
+            Console.WriteLine("yhteis pisteet on: " + pisteet);
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
